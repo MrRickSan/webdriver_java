7b1f7183fe23947d96cbafbcbac2d4b5f79471de
Mr Ricksan <ricardooliveira.info@gmail.com>
2020-12-03 23:38:48 +0000

    finding web elements

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,8 +1,11 @@
 package base;
 
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
 
@@ -16,7 +19,19 @@ public class BaseTests {
 
         // e.g. Iphone X size
         // driver.manage().window().setSize(new Dimension(375, 812));
-        driver.manage().window().maximize();
+        // driver.manage().window().maximize();
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+
+        WebElement shiftingContent = driver.findElement(By.linkText("Shifting Content"));
+        shiftingContent.click();
+
+        WebElement menu = driver.findElement(By.linkText("Example 1: Menu Element"));
+        menu.click();
+
+        List<WebElement> li = driver.findElements(By.xpath("//ul/li"));
+        System.out.println(li.size());
 
         System.out.println(driver.getTitle());
 
