9d6128ee0c7131e605cc5bfd3921d273ef851694
Mr Ricksan <ricardooliveira.info@gmail.com>
2020-12-10 00:04:47 +0000

    send keys

--- src/test/java/slider/HorizontalSliderTests.java
+++ src/test/java/slider/HorizontalSliderTests.java
@@ -3,11 +3,17 @@ package slider;
 import base.BaseTests;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class HorizontalSliderTests extends BaseTests {
 
     @Test
     public void testHorizontalSlider() {
         var sliderPage = homePage.clickHorizontalSlider();
-        sliderPage.setSliderValue("4");
+
+        String value = "4";
+        sliderPage.setSliderValue(value);
+
+        assertEquals(sliderPage.getSliderValue(), value, "Slider value is incorrect");
     }
 }
