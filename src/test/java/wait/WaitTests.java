fbe93d5aaac144fc7cf82b0c5e90c76f9b909c9f
Mr Ricksan <ricardooliveira.info@gmail.com>
2020-12-11 23:01:46 +0000

    wait strategies

--- src/test/java/wait/WaitTests.java
+++ src/test/java/wait/WaitTests.java
@@ -1,6 +1,7 @@
 package wait;
 
 import base.BaseTests;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -10,6 +11,13 @@ public class WaitTests extends BaseTests {
     @Test
     public void testWaitUntilHidden() {
         var loadingPage = homePage.clickDynamicLoading().clickExample1();
+        loadingPage.clickStart();
+        assertEquals(loadingPage.getLoadedText(), "Hello World!", "Loaded text incorrect");
+    }
+
+    @Test
+    public void testWaitUntilVisible() {
+        var loadingPage = homePage.clickDynamicLoading().clickExample2();
         loadingPage.clickStart();
         assertEquals(loadingPage.getLoadedText(), "Hello World!", "Loaded text incorrect");
     }
