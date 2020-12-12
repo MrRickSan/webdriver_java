09d5906179252b46855a07a51e21902bd8bda641
Mr Ricksan <ricardooliveira.info@gmail.com>
2020-12-12 01:34:38 +0000

    window manager

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.WindowManager;
 
 import java.util.concurrent.TimeUnit;
 
@@ -36,5 +37,9 @@ public class BaseTests {
         // closes the browser and ends the session.
         // p.s. close() closes the window but not necessarily the entire session.
         driver.quit();
+    }
+
+    public WindowManager getWindowManager() {
+        return new WindowManager(driver);
     }
 }
