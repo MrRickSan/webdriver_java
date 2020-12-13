6c206cc4b15ffbad7e8e5ef3986dc9bf5f95e6d5
Mr Ricksan <ricardooliveira.info@gmail.com>
2020-12-13 18:58:57 +0000

    selenium event listener

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,24 +1,35 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.EventReporter;
 import utils.WindowManager;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTests {
 
-    private WebDriver driver;
+//    private WebDriver driver;
+    private EventFiringWebDriver driver;
     protected HomePage homePage;
 
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new EventReporter());
 //        driver.manage().window().maximize();
         // Caution with implicitly waits because it can slow down all your tests by the number of interactions.
 //        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -30,6 +41,21 @@ public class BaseTests {
     @BeforeMethod
     public void goHome() {
         driver.get("https://the-internet.herokuapp.com/");
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result) {
+        // Will take screenshot only if the test is not successful
+        if (!result.isSuccess()) {
+            var camera = (TakesScreenshot)driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
+            try {
+                Files.move(screenshot, new File("resources/screenshots/" + result.getName()+ ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @AfterClass
